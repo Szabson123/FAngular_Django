7a459cf8eb62957f6580081666b20498482f450b
Szymon Żaba <szymonzaba18@gmail.com>
2024-03-31 20:26:46 +0200

    basic setup

--- FAngular_Django/FAngular_Django/asgi.py
+++ FAngular_Django/FAngular_Django/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FAngfular_Django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FAngular_Django.settings')
 
 application = get_asgi_application()
