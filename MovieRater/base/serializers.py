7ee5a7740213d827f6662e7d86bba5b7a1f3fdc3
Szymon Żaba <szymonzaba18@gmail.com>
2024-04-07 23:00:57 +0200

    more advanced serializer and add Ratings

--- MovieRater/base/serializers.py
+++ MovieRater/base/serializers.py
@@ -5,7 +5,7 @@ from base.models import Movie, Rating
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'number_of_ratings', 'avg_ratings')
 
 
 class RatingSerializer(serializers.ModelSerializer):
