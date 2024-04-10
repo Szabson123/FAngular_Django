d58684519e875fa7352cc3d0851059cf78e73780
Szymon Żaba <szymonzaba18@gmail.com>
2024-04-10 23:23:53 +0200

    bases of angular

--- angular-intro/src/app/app.component.ts
+++ angular-intro/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-intro';
+  name = 'Szymon';
+  wynik = 2+3+7+9;
+  counter = 0
+  people = [
+    {name: "Szymon", age: 25},
+    {name: "Natalia", age: 27}
+  ]
+
+  btnClicked = () =>{
+    console.log('button has been clicked');
+  }
+  counterClick = () =>{
+    this.counter ++;
+    console.log('button has been clicked', this.counter);
+  }
 }
