22a7f97b51b7502cc283d604814efcefff8ce6e1
Szymon Żaba <szymonzaba18@gmail.com>
2024-04-10 23:46:01 +0200

    Classes and styles

--- angular-intro/src/app/app.component.ts
+++ angular-intro/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-intro';
   name = 'Szymon';
   wynik = 2+3+7+9;
-  counter = 0
+  counter = 0;
+  mycolor = 'purple';
   people = [
     {name: "Szymon", age: 25},
     {name: "Natalia", age: 27}
   ]
+
+  ngOnInit() {
+    this.counter = 2;
+    console.log("Done")
+  }
 
   btnClicked = () =>{
     console.log('button has been clicked');
@@ -21,5 +27,9 @@ export class AppComponent {
   counterClick = () =>{
     this.counter ++;
     console.log('button has been clicked', this.counter);
+  }
+  changeColor = () =>{
+    this.mycolor = 'green'
+    console.log('Done')
   }
 }
