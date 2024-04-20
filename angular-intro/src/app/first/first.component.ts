56dd2eb072a4d63e3839cf5e186fd1873e15064f
Szymon Żaba <szymonzaba18@gmail.com>
2024-04-20 13:04:39 +0200

    forms

--- angular-intro/src/app/first/first.component.ts
+++ angular-intro/src/app/first/first.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrl: './first.component.css'
+  styleUrls: ['./first.component.css']  // Poprawione na 'styleUrls'
 })
 export class FirstComponent {
+  ourForm = new FormGroup({  
+    name: new FormControl(''),
+    surname: new FormControl('')  
+  });
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+  sendForm = () =>{
+    console.log(this.ourForm.value)
+  }
 
 }
